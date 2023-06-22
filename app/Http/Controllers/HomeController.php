e2dc0543d72f7142091afc4aab15891e1861ddf5
smsibghatullah <smsibghatullah@gmail.com>
2023-06-22 12:46:15 +0500

    point issues issaues

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3556,6 +3556,7 @@ $getresult = $result;
       $team_wins = isset($match_count_winning_team[$team_id]) ? $match_count_winning_team[$team_id] : 0;
       $team_losses = isset($match_count_loss_team[$team_id]) ? $match_count_loss_team[$team_id] : 0;
       $team_total_matches = isset($match_count_team_a[$team_id]) ? $match_count_team_a[$team_id] : 0;
+      $team_tie = isset($match_count_tie_team[$team_id]) ? $match_count_tie_team[$team_id] : 0;
 
       if (isset($match_count_team_b[$team_id])) {
         $team_total_matches += $match_count_team_b[$team_id];
